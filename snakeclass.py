f0f3b497b01d3b0e9a59d4d5abe0e914f44504e0
KinyaElGRande <kinya.francis.mumbi@gmail.com>
2019-10-03 15:55:27 +0300

    using init function

--- snakeclass.py
+++ snakeclass.py
@@ -11,3 +11,22 @@ print(snake.name)
 
 snake.change_name("Python")
 print(snake.name)
+
+
+#----------------another way of doing it is by defining attributes in init method --------------------------------|
+
+class Snake:
+
+    def __init__(self, name):
+        self.name = name
+
+    def change_name(self, new_name):
+        self.name = new_name
+
+blackmamba = Snake("black mamba")
+python = Snake("python")
+
+print(blackmamba.name)
+
+print(python.name)
+        
